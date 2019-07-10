540752fcc429f029e13207e36683d06b95f697f7
Terminal-Redux <52044033+Terminal-Redux@users.noreply.github.com>
2019-07-10 08:27:53 -0500

    Add files via upload

---
+++ packages/custombundles/helloworld/bundle.js
@@ -0,0 +1,62 @@
+/**
+ * Definition for bundle. See source for details.
+ *
+ * @class Oskari.custombundles.helloworld.MyBundle
+ */
+Oskari.clazz.define("Oskari.custombundles.helloworld.MyBundle",
+
+    /**
+     * Called automatically on construction. At this stage bundle sources have been
+     * loaded, if bundle is loaded dynamically.
+     *
+     * @contructor
+     * @static
+     */
+    function() {
+
+    }, {
+        /*
+         * called when a bundle instance will be created
+         *
+         * @method create
+         */
+        "create": function() {
+            return Oskari.clazz.create("Oskari.custombundles.helloworld.MyBundleInstance");
+        },
+        /**
+         * Called by Bundle Manager to provide state information to
+         *
+         * @method update
+         * bundle
+         */
+        "update": function(manager, bundle, bi, info) {}
+    },
+
+    /**
+     * metadata
+     */
+    {
+        "protocol": ["Oskari.bundle.Bundle"],
+        "source": {
+            "scripts": [{
+                "type": "text/javascript",
+                "src": "../../../bundles/custombundles/helloworld/instance.js"
+            }],
+            "locales": [{
+                "lang": "fi",
+                "type": "text/javascript",
+                "src": "../../../bundles/custombundles/helloworld/resources/locale/fi.js"
+            }, {
+                "lang": "sv",
+                "type": "text/javascript",
+                "src": "../../../bundles/custombundles/helloworld/resources/locale/sv.js"
+            }, {
+                "lang": "en",
+                "type": "text/javascript",
+                "src": "../../../bundles/custombundles/helloworld/resources/locale/en.js"
+            }]
+        }
+    });
+
+// Install this bundle by instantating the Bundle Class
+Oskari.bundle_manager.installBundleClass("helloworld", "Oskari.custombundles.helloworld.MyBundle");
